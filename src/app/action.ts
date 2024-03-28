0a150fa736952daae312060c614b9dbd3d3c4e93
sameerchauhan1 <sameerchauhan11o8@gmail.com>
2024-03-29 01:39:10 +0530

    setup context-provider

---
+++ src/app/action.ts
@@ -0,0 +1,32 @@
+"use server";
+
+import { currentUser } from "@clerk/nextjs/server";
+import { StreamClient } from "@stream-io/node-sdk";
+
+export async function getToken() {
+  const streamApiKey = process.env.NEXT_PUBLIC_STREAM_VIDEO_API_KEY;
+  const streamApiSecret = process.env.STREAM_VIDEO_API_SECRET;
+
+  if (!streamApiKey || !streamApiSecret) {
+    throw new Error("Missing Stream Video API Key or Secret");
+  }
+
+  const user = await currentUser();
+
+  console.log("Generating token for the user: ", user?.id);
+
+  if (!user) {
+    throw new Error("User not found");
+  }
+
+  const streamClient = new StreamClient(streamApiKey, streamApiSecret);
+
+  const expirationTime = Math.floor(Date.now() / 1000) + 60 * 60;
+  const issuedAt = Math.floor(Date.now() / 1000) - 60;
+
+  const token = streamClient.createToken(user.id, expirationTime, issuedAt);
+
+  console.log("Generated token: ", token);
+
+  return token;
+}
